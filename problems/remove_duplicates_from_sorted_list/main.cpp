8de8b884c8915cac2870288fbb54ea72122e9af2
yzheng51 <y.zheng211@gmail.com>
2021-08-13 21:50:33 +0800

    Update code

--- problems/remove_duplicates_from_sorted_list/main.cpp
+++ problems/remove_duplicates_from_sorted_list/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[]) {
     ListNode *second = new ListNode(1, third);
     ListNode *first = new ListNode(1, second);
 
-    Solution sln = Solution();
+    Solution sln;
     auto res = sln.deleteDuplicates(first);
 
     while (res != nullptr) {
